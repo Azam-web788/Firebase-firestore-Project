90fd3213f26fa7fa73d3ef828c0c292ca9c73fae
Azam-web778 <asifanadeem482@gmail.com>
2025-02-04 08:38:55 -0800

    firestore-project

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -1,4 +1,3 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
@@ -8,13 +7,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/11.2.0/firebase
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC7sYJVABzTaT0sGp_Jbk8b8FFu2aPJIdE",
-  authDomain: "firestore-c1495.firebaseapp.com",
-  projectId: "firestore-c1495",
-  storageBucket: "firestore-c1495.firebasestorage.app",
-  messagingSenderId: "466207389273",
-  appId: "1:466207389273:web:f947343ca6296fa0792348",
-  measurementId: "G-MDKHGVGE9M"
+  apiKey: "AIzaSyBgWTog9ZQPCSxRs3qgjKqXapc9GfH7_F4",
+  authDomain: "firestore-2821a.firebaseapp.com",
+  projectId: "firestore-2821a",
+  storageBucket: "firestore-2821a.firebasestorage.app",
+  messagingSenderId: "525076232407",
+  appId: "1:525076232407:web:6668930bef24e573c15892",
+  measurementId: "G-T58Y468M0F"
 };
 
 // Initialize Firebase
